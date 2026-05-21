31e86f71e661ba1b26833005c90afc8908993e9e
Shreya Garud <shreyagarud4@gmail.com>
2026-05-22 00:26:18 +0530

    second commit

---
+++ to do application/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h1 style={{textAlign:"center",color:"blue"}}>To Do Application</h1>
+  )
+}
+
+export default Header
---
+++ to do application/src/data.js
@@ -0,0 +1,4 @@
+export const tasks  = [
+    {id:1, Title:"React", description:"React is a JS library", status:"isCompleterd"},
+    {id:2, Title:"Node.js", description:"React node", status:"inprogress"}
+]
