3c20f118611dd39fd4e143702c7aaf618e2cb1e0
ShreyaGarud <shreyagarud4@gmail.com>
2026-05-22 19:00:26 +0530

    Delete to do application directory

--- to do application/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () => {
-  return (
-    <h1 style={{textAlign:"center",color:"blue"}}>To Do Application</h1>
-  )
-}
-
-export default Header
--- to do application/src/data.js
+++
@@ -1,4 +0,0 @@
-export const tasks  = [
-    {id:1, Title:"React", description:"React is a JS library", status:"isCompleterd"},
-    {id:2, Title:"Node.js", description:"React node", status:"inprogress"}
-]
